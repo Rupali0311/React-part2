2bdf6980dd21945e90fcf9b7bd86ffc22d42e71a
Rupali0311 <rupalitiwary0311@gmail.com>
2021-11-23 23:49:09 +0530

    upto data fetching done

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import MeetupList from '../components/meetups/MeetupList';
 
 const DUMMY_DATA = [
@@ -22,6 +24,24 @@ const DUMMY_DATA = [
 ];
 
 function AllMeetupsPage() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  fetch(
+    'https://react-getting-started-5a43f-default-rtdb.firebaseio.com/meetups.json'
+  ).then(response => {
+    return response.json();
+  }).then(data => {
+    setIsLoading(false);
+  });
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
     return (
         <section>
             <h1>All Meetups</h1>
